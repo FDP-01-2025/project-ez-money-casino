b7377fa3bec84c5d4b6c4b0419b8ab0ebd1799ef
Ialx-mtz <00053325@uca.edu.sv>
2025-06-10 19:25:09 -0600

    menu update :memo:

---
+++ menu.h
@@ -0,0 +1,15 @@
+#ifndef menu_h
+#define menu_h
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void showWelcome(){
+    cout << " + - + - + - + - + - + - + - + " << endl;
+    cout << " | W | E | L | C | O | M | E | " << endl;
+    cout << " + - + - + - + - + - + - + - + " << endl;
+
+    
+}
